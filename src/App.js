06279fc1c2a8564cfb28dde7b83cd2be96e63d59
unknown <hninhnin0929>
2023-05-06 14:34:22 +0900

    add analytics report line chart

--- src/App.js
+++ src/App.js
@@ -13,9 +13,10 @@ import ThemeCustomization from './themes';
 import  ShowCard from './components/cards/ShowCard';
 import  MainCard from './components/cards/MainCard';
 import OrderTable from './components/pages/OrderTable'
+import ReportAreaChart from './components/pages/ReportAreaChart'
 import { ThemeProvider, createTheme } from '@mui/system';
 import { Grid,Box,Card,  CardContent,CardActions, Button, Typography,Stack,
-  Chip } from '@mui/material';
+  Chip, List, ListItemButton,ListItemText, MenuItem } from '@mui/material';
 
 function App() {
   return (
@@ -52,6 +53,32 @@ function App() {
             </Grid>
             <MainCard sx={{ mt: 2 }} content={false}>
                 <OrderTable />
+            </MainCard>
+          </Grid>
+          
+          <Grid item xs={12} md={5} lg={4}>
+            <Grid container alignItems="center" justifyContent="space-between">
+              <Grid item>
+                  <Typography variant="h5">Analytics Report</Typography>
+              </Grid>
+              <Grid item />
+            </Grid>
+            <MainCard sx={{ mt: 2 }} content={false}>
+              <List sx={{ p: 0, '& .MuiListItemButton-root': { py: 2 } }}>
+                <ListItemButton divider>
+                  <ListItemText primary="Company Finance Growth" />
+                  <Typography variant="h5">+45.14%</Typography>
+                </ListItemButton>
+                <ListItemButton divider>
+                    <ListItemText primary="Company Expenses Ratio" />
+                    <Typography variant="h5">0.58%</Typography>
+                </ListItemButton>
+                <ListItemButton>
+                    <ListItemText primary="Business Risk Cases" />
+                    <Typography variant="h5">Low</Typography>
+                </ListItemButton>
+              </List>
+              <ReportAreaChart />
             </MainCard>
           </Grid>
       </Grid>
