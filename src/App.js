468a87b8b89416d46e5499548529cb62256385a6
unknown <hninhnin0929>
2023-05-06 23:37:36 +0900

    add help and support chat using avatar

--- src/App.js
+++ src/App.js
@@ -20,10 +20,13 @@ import SalesColumnChart from './components/pages/SalesColumnChart';
 import { ThemeProvider, createTheme } from '@mui/system';
 import { GiftOutlined, MessageOutlined, SettingOutlined } from '@ant-design/icons';
 import { Grid,Box,Card,  CardContent,CardActions, Button, Typography,Stack,
-  Chip, List, ListItemButton,ListItemText, MenuItem, TextField, Avatar,
+  Chip, List, ListItemButton,ListItemText, MenuItem, TextField, Avatar,AvatarGroup,
   ListItemAvatar, ListItemSecondaryAction } from '@mui/material';
 
-
+import avatar1 from './assets/images/users/avatar-1.png';
+import avatar2 from './assets/images/users/avatar-2.png';
+import avatar3 from './assets/images/users/avatar-3.png';
+import avatar4 from './assets/images/users/avatar-4.png';
   // sales report status
 const status = [
   {
@@ -55,6 +58,7 @@ const actionSX = {
   alignSelf: 'flex-start',
   transform: 'none'
 };
+
 
 function App() {
   const [value, setValue] = useState('today');
@@ -250,6 +254,33 @@ function App() {
 
                     </List>
                   </MainCard>
+                  <MainCard sx={{ mt: 2 }}>
+                    <Stack spacing={3}>
+                        <Grid container justifyContent="space-between" alignItems="center">
+                            <Grid item>
+                                <Stack>
+                                    <Typography variant="h5" noWrap>
+                                        Help & Support Chat
+                                    </Typography>
+                                    <Typography variant="caption" color="secondary" noWrap>
+                                        Typical replay within 5 min
+                                    </Typography>
+                                </Stack>
+                            </Grid>
+                            <Grid item>
+                                <AvatarGroup sx={{ '& .MuiAvatar-root': { width: 32, height: 32 } }}>
+                                    <Avatar alt="Remy Sharp" src={avatar1} />
+                                    <Avatar alt="Travis Howard" src={avatar2} />
+                                    <Avatar alt="Cindy Baker" src={avatar3} />
+                                    <Avatar alt="Agnes Walker" src={avatar4} />
+                                </AvatarGroup>
+                            </Grid>
+                        </Grid>
+                        <Button size="small" variant="contained" sx={{ textTransform: 'capitalize' }}>
+                            Need Help?
+                        </Button>
+                    </Stack>
+                </MainCard>
             </Grid>            
 
       </Grid>
